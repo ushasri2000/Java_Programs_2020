2d18c41907c3b048d5fd8bf45f04240d2f2b3a24
ushasri2000 <69568438+ushasri2000@users.noreply.github.com>
2020-08-30 11:36:39 +0530

    Add files via upload

---
+++ Nearest_Prime.java
@@ -0,0 +1,88 @@
+package pack1;
+import java.util.*;
+
+public class Nearest_Prime {
+	boolean isPrime(int n)  //n = 20
+	{
+		int c=0;
+		for(int i=2; i<n; i++) // omitted 1 and 20
+		{
+			if(n%i == 0)  //  20/2 == 0 -> t
+				c++;  // c = value 
+		}
+		if (c==0)
+			return true; // for 19 true
+		else
+			return false; // for 20 false
+	}
+	
+	int beforePrime(int n)  // n= 20
+	{
+		int c=0;
+		while(true)
+		{
+			if(isPrime(n))    // n = 19
+				return c;   // c=1
+			else
+			{
+				n = n-1;  // n=19
+				c++;  // c= 1
+			}
+				
+		}
+	}
+	
+	int afterPrime(int n)  //n = 20
+	{
+		int c=0;
+		while(true)
+		{
+			if(isPrime(n))  //n= 23 -> false
+				return c; // c = 3
+			else
+			{
+				n = n+1; // n =23
+				c++; // c=3
+			}
+		}
+	}
+	
+	void nearestPrimes()
+	{
+		Scanner obj = new Scanner(System.in);
+		System.out.println("enter number:");
+		int n = obj.nextInt();  // n = 20
+	    int m1 = beforePrime(n);  // m1 = 1
+	    int m2 = afterPrime(n);  // m2 = 3
+	    
+	    if(m1>m2)  // 1>3 -> f
+	    {
+	    	System.out.println("Nearest prime:");
+	    	System.out.println(n+m2);
+	    }
+	    else if(m2>m1)  // 3>1 -> t
+	    {
+	    	System.out.println("Nearest prime:");
+	    System.out.println(n-m1);  // 20-1 = 19
+	    }
+	    else if((n-m1)==(n+m2))
+	    	System.out.println(n+" is prime number");
+
+	    else if(m1 == m2 )
+	    {
+	    	System.out.println("Nearest primes are:");
+		    System.out.println((n-m1)+" "+(n+m2));
+	    }   
+	    
+	}	
+	
+	
+	public static void main(String[] args) 
+	{
+	Nearest_Prime obj = new Nearest_Prime();
+	obj.nearestPrimes();
+	
+
+	}
+
+}
---
+++ Reverse_Number.java
@@ -0,0 +1,20 @@
+package pack1;
+import java.util.*;
+
+public class Reverse_Number {
+
+	public static void main(String[] args) 
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter number:");
+	int num = sc.nextInt();  //1234
+	int rev=0;
+	
+	while(num != 0)  // 0
+	{
+		int d = num%10;  // 1 (rem) // last digit of a number
+		rev = rev*10 + d;  // 432*10 + 1 = 4321  //first digit in rev var
+		num = num /10; // 1/10 = 0(quo) omitting last digits from num var
+	}
+	System.out.println("reverse number = "+ rev);		
+	}	}
