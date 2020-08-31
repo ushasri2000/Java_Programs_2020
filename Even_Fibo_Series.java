97a7441c08c38515a84aeb6b62e2be90a9f7bf9d
ushasri2000 <69568438+ushasri2000@users.noreply.github.com>
2020-08-31 19:55:37 +0530

    Add files via upload

---
+++ Even_Fibo_Series.java
@@ -0,0 +1,26 @@
+package pack1;
+import java.util.*;
+
+public class Even_Fibo_Series {
+
+	public static void main(String[] args) 
+	{
+	Scanner sc = new Scanner(System.in);
+	System.out.println("enter number:");
+	int n = sc.nextInt();  // 10	
+	int t1 = 0, t2 =1;
+	System.out.println("Fibonacci Series:");
+	
+	for(int i = 1; i<n ; i++)
+	{
+		int sum = t1 + t2;  //0+1 = 1
+		t1 = t2; // t1 = 1
+		t2 = sum;  // t2 =1 
+		//System.out.print(sum+" ");
+		if(t1>0)
+		{
+			if(t1%2==0 && t1<n)
+				System.out.print(t1+" ");
+		}
+	}	
+} }
---
+++ Fibo_Num.java
@@ -0,0 +1,25 @@
+package pack1;
+import java.util.*;
+
+public class Fibo_Num {
+
+	public static void main(String[] args) 
+	{
+	Scanner sc = new Scanner (System.in);
+	System.out.println("enter number");
+	int n = sc.nextInt();  //5
+	
+	int a=0,b=1,c=0;
+	
+	while(c<n)   // 5<5
+	{
+		c=a+b;  // 5
+		a=b;  // 3
+		b=c;  // 5
+	}
+     if (c==n)
+    	 System.out.println("Fibonacci Number");
+     else
+    	 System.out.println("Not Fibonacci Number");	
+	}
+}
