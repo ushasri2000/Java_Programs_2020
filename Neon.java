a49814e7e6a2400e57f9be380a444f278a9186bc
ushasri2000 <69568438+ushasri2000@users.noreply.github.com>
2020-09-10 19:32:53 +0530

    Add files via upload

---
+++ Neon.java
@@ -0,0 +1,20 @@
+package pack1;
+import java.util.*;
+public class Neon {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter number :");
+		int n = sc.nextInt(); // n = 9
+		int sq = n*n; // sq = 9*9 = 81
+		int sum =0;
+		while(sq > 0 ) // 8 >0
+		{
+			int rem = sq%10; // rem = 8%10 = 8
+			sum = sum + rem ; // sum = 1+ 8 = 9
+			sq = sq/10; // sq = 8/10 = 0
+		}
+		if (sum == n)
+			System.out.println("Neon Number");
+		else
+			System.out.println("Not Neon Number");
+		} }
