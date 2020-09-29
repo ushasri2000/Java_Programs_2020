2250974936eee0de0f296ff1921b22f26cc78bd8
ushasri2000 <69568438+ushasri2000@users.noreply.github.com>
2020-09-29 19:06:21 +0530

    Add files via upload

---
+++ Palindrome_Range.java
@@ -0,0 +1,28 @@
+package pack1;
+import java.util.*;
+public class Palindrome_Range {
+	static int isPalindrome(int n) //n=101
+	{
+		int rev =0;
+		for(int i=n; i>0; i/=10) // i=1; ; 1/10 = 0
+		{
+		   rev = rev*10 + i%10; // rev = 10*10 +1%10 = 1 =101		   
+		}
+		return (n==rev) ? 1 : 0;		
+	}	
+	
+	static void display(int n1,int n2)
+	{
+		for(int i = n1; i <= n2; i++)
+			if(isPalindrome(i)==1)
+				System.out.print(i + " ");
+	}
+	
+	public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("enter 2 numbers : ");
+    int n1 = sc.nextInt(); // n1 = 100
+    int n2 = sc.nextInt(); // n2 = 200
+     display(n1,n2);
+	} }
+
