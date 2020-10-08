2845357a11531beb75791729beeffbcf61636f66
ushasri2000 <69568438+ushasri2000@users.noreply.github.com>
2020-10-08 21:54:02 +0530

    Add files via upload

---
+++ Pattern_40.java
@@ -0,0 +1,20 @@
+package pack2;
+
+public class Pattern_40 {
+
+	public static void main(String[] args) {
+		int n =5;
+		int x=1;
+		
+	for(int i=1; i<=n; i++) // i = 1.. 5 rows
+	{
+		for(int j =n-1; j>= i; j--) // j = 4..1		
+				System.out.print(" "); // 4 spaces
+					
+		for(int k=x; k>=1; k-- ) // k =1,3,5,7,9
+			System.out.print(k); // 1
+				
+		System.out.println();
+		x= x+2; // x = 3,5,7,9		
+	}
+	}}
