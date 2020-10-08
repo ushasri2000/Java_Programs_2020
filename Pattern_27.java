91ee498f552ce48a7721bf5d63e4630e574103f2
ushasri2000 <69568438+ushasri2000@users.noreply.github.com>
2020-10-08 21:53:21 +0530

    Add files via upload

---
+++ Pattern_27.java
@@ -0,0 +1,18 @@
+package pack2;
+
+public class Pattern_27 {
+
+	public static void main(String[] args) {	
+		
+		for(int i='A'; i <= 'E' ; i++)// 5 rows
+		{
+			for(int j= 'E'-1; j >= i ; j--)// j=68 	>= 65 	
+				System.out.print(" "); // 4,3,2,1,0 - > spaces
+			
+			for(int k='A'; k <= i; k++) // A <= A -> 1 repeating
+				System.out.print((char)i); // A
+			
+			System.out.println();			
+		}
+	}}
+
