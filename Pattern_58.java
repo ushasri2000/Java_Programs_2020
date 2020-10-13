fde9c9a544d6e9db710853a4a805312a0d4d8d79
ushasri2000 <69568438+ushasri2000@users.noreply.github.com>
2020-10-13 18:18:44 +0530

    Add files via upload

---
+++ Pattern_58.java
@@ -0,0 +1,18 @@
+package pack2;
+
+public class Pattern_58 {
+
+	public static void main(String[] args) {
+		int n = 3;
+		for(int i = n; i >= -n; i--) // i= 3,2,1,0,-1,-2,-3
+		{
+			for(int j=1; j<=Math.abs(i); j++)// j = 1<=0
+			 System.out.print(" "); // 3,2,1,0,1,2,3 spaces
+			
+			for(int k=n; k>= Math.abs(i);k--)// k= 3>=3
+				System.out.print(k); // 1,2,3,4,3,2,1 star
+				
+				System.out.println();
+		}
+	}
+}
---
+++ Pattern_65.java
@@ -0,0 +1,20 @@
+package pack2;
+
+public class Pattern_65 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i = 1; i<= n; i++)  // i = 1, 2,3,4,5
+		{
+			for(int k = n-1; k >= i; k--) // k =4 > = 5
+			{
+				System.out.print(" "); // 4,3,2,1,0 -> spaces
+			}
+			for(int j = 1; j <=i ; j++ ) // 1<=5
+			{
+				System.out.print((char)(i+64)+" "); // 1,2,3,4,5 -> star
+			}
+			System.out.println();
+		}
+}}
+
