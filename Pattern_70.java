58756be7c68bdf95bcb6390e363db945fc31a655
ushasri2000 <69568438+ushasri2000@users.noreply.github.com>
2020-10-16 19:44:47 +0530

    Add files via upload

---
+++ Pattern_70.java
@@ -0,0 +1,17 @@
+package pack2;
+
+public class Pattern_70 {
+
+	public static void main(String[] args) {
+		int n = 5;
+		for(int i = n ; i >= 1; i--) // i = 5, 4, 3,2,1 rows
+		{
+			for(int j = n-1;  j >= i; j--) // j =4 >= 4 -> t
+				System.out.print(" "); // 0,1,2,3,4
+			for(int k = i; k >= 1; k--) // k =4,3,2,1
+				System.out.print(i+" "); //5,4,3,2,1
+			
+			System.out.println();			
+		}
+}	}
+
